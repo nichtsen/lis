e71af6e80ff4be92dca202657a722d2dbbbd3726
nichtsen <hobblong@163.com>
2021-01-23 19:00:57 +0800

    adapt to doc

--- rational/rational_test.go
+++ rational/rational_test.go
@@ -2,7 +2,7 @@ package rational
 
 import "fmt"
 
-func ExampleOps() {
+func Example_Ops() {
 	a := New(1, 2)
 	b := New(1, 3)
 	fmt.Printf("%v %v %v %v %v %v", a, b, Add(a, b), Subtract(a, b), Multiply(a, b), Divide(a, b))

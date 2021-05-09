019eba9ea4a350b7d979733553808042e69b9133
nichtsen <hobblong@163.com>
2021-05-10 03:36:59 +0800

    code table

--- huffman/huffman.go
+++ huffman/huffman.go
@@ -1,8 +1,13 @@
 package huffman
 
+import (
+	"fmt"
+	"sort"
+)
+
 type Huffman struct {
-	sybomls string
-	weight  int
+	sybomls []string
+	weight  uint
 	lnode   *Huffman
 	rnode   *Huffman
 }
@@ -22,7 +27,7 @@ type coder struct {
 }
 
 func (c *coder) read() int {
-	if c.offset/8 > len(c.buf) {
+	if c.offset/8 > len(c.buf)-1 {
 		return -1
 	}
 	defer func() {
@@ -38,14 +43,83 @@ func (c *coder) read() int {
 	return 1
 }
 
-func (h *Huffman) Decode(b []byte) string {
+// New reutrn a Huffman node
+func New(symbol string, weight uint) *Huffman {
+	tmp := make([]string, 0, 1)
+	tmp = append(tmp, symbol)
+	return &Huffman{
+		sybomls: tmp,
+		weight:  weight,
+	}
+}
+
+// Merge merge two Huffman nodes into one
+// a is implicitly smaller or equal to b;
+// a leaf node always on the left branch
+func Merge(a, b *Huffman) *Huffman {
+	tmp := make([]string, 0, len(a.sybomls)+len(b.sybomls))
+	tmp = append(tmp, b.sybomls...)
+	tmp = append(tmp, a.sybomls...)
+	if a.IsLeaf() {
+		return &Huffman{
+			sybomls: tmp,
+			weight:  a.weight + b.weight,
+			lnode:   a,
+			rnode:   b,
+		}
+	}
+	if b.IsLeaf() {
+		return &Huffman{
+			sybomls: tmp,
+			weight:  a.weight + b.weight,
+			lnode:   b,
+			rnode:   a,
+		}
+	}
+	return &Huffman{
+		sybomls: tmp,
+		weight:  a.weight + b.weight,
+		lnode:   a,
+		rnode:   b,
+	}
+}
+
+// NewHuffman construct a Huffman tree and reutrn the root
+// of it from the input data
+// the input data is a sequence of Huffman leaf node
+func NewHuffman(set []*Huffman) *Huffman {
+	if len(set) == 0 {
+		return nil
+	}
+	if len(set) == 1 {
+		return set[0]
+	}
+
+	sort.Slice(set, func(i, j int) bool {
+		return set[i].weight > set[j].weight
+	})
+	var cursor *Huffman
+	for len(set) > 1 {
+		cursor = Merge(set[len(set)-1], set[len(set)-2])
+		set = set[:len(set)-1]
+		set[len(set)-1] = cursor
+		sort.Slice(set, func(i, j int) bool {
+			return set[i].weight > set[j].weight
+		})
+	}
+	return cursor
+}
+
+// Decode
+func (h *Huffman) Decode(b []byte) []string {
 	c := new(coder)
+	c.buf = make([]byte, len(b))
 	copy(c.buf, b)
-	var res string
+	var res []string
 	current := h
-	for i := 0; i < len(c.buf); i++ {
-		if h.IsLeaf() {
-			res += h.sybomls
+	for i := 0; i < len(c.buf)*8; i++ {
+		if current.IsLeaf() {
+			res = append(res, current.sybomls...)
 			current = h
 			continue
 		}
@@ -53,12 +127,12 @@ func (h *Huffman) Decode(b []byte) string {
 		if bit == -1 {
 			break
 		}
-		current = h.Next(bit)
+		current = current.Next(bit)
 	}
-	_ = current
 	return res
 }
 
+// Next return the next node accoriding to the current bit
 func (h *Huffman) Next(b int) *Huffman {
 	if b == 0 {
 		return h.lnode
@@ -66,9 +140,59 @@ func (h *Huffman) Next(b int) *Huffman {
 	return h.rnode
 }
 
+// IsLeaf predicate if the current node if a leaf
 func (h *Huffman) IsLeaf() bool {
-	if len(h.sybomls) == 1 {
+	if len(h.sybomls) == 1 && h.lnode == nil && h.rnode == nil {
 		return true
 	}
 	return false
+}
+
+func (h *Huffman) CodeTable() map[string][]string {
+	res := make(map[string][]string)
+	if h.IsLeaf() {
+		return nil
+	}
+	hd := func(node *Huffman, bits int) {
+		if node.IsLeaf() {
+			res[fmt.Sprintf("%b", bits)] = node.sybomls
+		}
+	}
+	tr := NewTraversal(hd, h)
+	tr.PreOrder(h, 0)
+	return res
+}
+
+func (h *Huffman) String() string {
+	if h.IsLeaf() {
+		return fmt.Sprintf("--Huffman Leaf(symbol: %v, weight: %v)", h.sybomls, h.weight)
+	}
+	return fmt.Sprintf("--Huffman Node(symbol: %v, weight: %v, \n -->left node(%v) -->right node(%v))", h.sybomls, h.weight, h.lnode, h.rnode)
+}
+
+func (h *Huffman) codeTable() map[string]string {
+	return map[string]string{}
+}
+
+type Traversal struct {
+	root    *Huffman
+	handler func(node *Huffman, baseBits int)
+}
+
+func NewTraversal(handler func(*Huffman, int), root *Huffman) *Traversal {
+	return &Traversal{
+		root:    root,
+		handler: handler,
+	}
+
+}
+
+// PreOrder Traversal
+func (tr *Traversal) PreOrder(node *Huffman, baseBits int) {
+	if node == nil {
+		return
+	}
+	tr.handler(node, baseBits)
+	tr.PreOrder(node.lnode, baseBits<<1)
+	tr.PreOrder(node.rnode, (baseBits<<1)+1)
 }
--- huffman/huffman_test.go
+++ huffman/huffman_test.go
@@ -1,13 +1,84 @@
 package huffman
 
 import (
+	"encoding/binary"
 	"fmt"
 	"testing"
 )
 
 func TestA(t *testing.T) {
 	fmt.Println(0b100)
+	fmt.Printf("%b\n", 0b110)
 	fmt.Println(32 & 01 << 2)
 	var str = "abc"
 	fmt.Println(len(str))
+	tmp := make([]int, 10)
+	fmt.Println("---")
+	for len(tmp) > 1 {
+		tmp = tmp[:len(tmp)-1]
+		fmt.Println(len(tmp))
+	}
+}
+
+func TestConsturctor(t *testing.T) {
+	input := map[string]uint{
+		"A": 8,
+		"B": 3,
+		"C": 1,
+		"D": 1,
+		"E": 1,
+		"F": 1,
+		"G": 1,
+		"H": 1,
+	}
+	var set []*Huffman
+	for symbol, weight := range input {
+		set = append(set, New(symbol, weight))
+	}
+	// fmt.Println(set)
+	h := NewHuffman(set)
+	fmt.Printf("root: %v\n", h)
+	tab := h.CodeTable()
+	fmt.Println(tab)
+	bee := []byte{0b11010011, 0b00100000}
+
+	str := h.Decode(bee)
+	fmt.Println(str)
+}
+
+func TestC(t *testing.T) {
+	bee := []byte{0b11010011, 0b00100000}
+	coder := new(coder)
+	coder.buf = bee
+	fmt.Println(len(coder.buf))
+	for {
+		i := coder.read()
+		fmt.Printf("%d", i)
+		if i == -1 {
+			break
+		}
+	}
+}
+
+func TestCoder(t *testing.T) {
+	bee := int64(0b11011111)
+	buf := make([]byte, binary.MaxVarintLen64)
+	n := binary.PutVarint(buf, bee)
+	fmt.Println(n)
+	bin := binary.LittleEndian.Uint64(buf)
+	buf = buf[:2]
+	fmt.Printf("%b\n", bee)
+	fmt.Printf("%b\n", bin)
+	fmt.Printf("%b | %b\n", buf[0], buf[1])
+	coder := new(coder)
+	coder.buf = buf
+	fmt.Println(len(coder.buf))
+
+	for {
+		i := coder.read()
+		fmt.Printf("%d", i)
+		if i == -1 {
+			break
+		}
+	}
 }
